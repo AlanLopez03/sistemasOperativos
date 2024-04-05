709df9b90a8bbd2de977068c191052e8003e3c59
AlanLopez03 <lora030325@gs.utm.mx>
2024-04-05 16:41:02 -0700

    xd

--- serie.c
+++ serie.c
@@ -1,124 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-char **separarComandos(char *linea)
-{
-    char delimitadores[] = "|><";
-    char *token;
-    char **comandos = (char **)malloc(255 * sizeof(char *));
-    int i = 0;
-    token = strtok(linea, delimitadores);
-    while (token != NULL)
-    {
-        comandos[i] = token;
-        i++;
-        token = strtok(NULL, delimitadores);
+#include <stdlib.h>
+
+int main() {
+    pid_t pid = fork();
+if (pid == 0) {
+        // Proceso hijo termina inmediatamente.
+        exit(0);
     }
-    return comandos;
-}
-char **separarArgumentos(char *comando)
-{
-    char delimitadores[] = " ";
-    char *token;
-    char **argumentos = (char **)malloc(255 * sizeof(char *));
-    int i = 0;
-    token = strtok(comando, delimitadores);
-    while (token != NULL)
-    {
-        argumentos[i] = token;
-        i++;
-        token = strtok(NULL, delimitadores);
-    }
-    return argumentos;
-}
-int contarElementos(char **argumentos)
-{
-    int i = 0;
-    while (argumentos[i] != NULL)
-        i++;
+    else {
+        // Proceso padre duerme por un tiempo y termina sin esperar al hijo.
+        sleep(20);
+    } 
 
-    return i;
-}
-
-void ponerFinCadena(char *linea)
-{
-    // printf("dd=%s\n", linea);
-    linea[strcspn(linea, " ")] = '\0';
-}
-
-char *quitarSalto(char *linea)
-{
-    linea[strcspn(linea, "\n")] = '\0';
-    return linea;
-}
-char *getSeparadores(char *linea)
-{
-    char *separadores = (char *)malloc(255 * sizeof(char));
-    int j = 0;
-    for (int i = 0; i < strlen(linea); i++)
-    {
-        if (linea[i] == '|' || linea[i] == '<' || linea[i] == '>')
-        {
-            separadores[j] = linea[i];
-            j++;
-        }
-    }
-
-    separadores[j] = '\0';
-    return separadores;
-}
-
-
-
-
-int main(){
-
-    int tuberias[1][2];
-    char *comandos[] = {"ls","wc"};
-    int n = 2;
-    char **argumentos;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (pipe(tuberias[i]) == -1)
-        {
-            perror("Error al crear la tubería");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    for(int i=0;i<n;i++){
-        int pid = fork();
-         if(pid == 0){
-            if(i > 0){
-                dup2(tuberias[i - 1][0], STDIN_FILENO);
-            }
-            if(i < n - 1){
-                dup2(tuberias[i][1], STDOUT_FILENO);
-            }
-            for (int j = 0; j < n - 1; j++)
-            {
-                close(tuberias[j][0]);
-                close(tuberias[j][1]);
-            }
-            //argumentos=separarArgumentos(comandos[i]);
-            //printf("argumentos=%s\n",argumentos[0]);
-            execlp(comandos[i], comandos[i], NULL);
-            perror("Error al ejecutar el comando");
-            exit(EXIT_FAILURE);
-        }else{
-            if(i > 0){
-                close(tuberias[i - 1][0]);
-            }
-            if(i < n - 1){
-                close(tuberias[i][1]);
-            }
-            wait(NULL);
-        }
-
-    }
-
+    return 0;
 }
